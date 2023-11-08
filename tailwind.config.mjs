12b888ea32004fec4924b11e35ca9f2a6bd280e7
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2023-11-08 14:43:43 +0530

    fix 1

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	mode: 'jit',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	darkMode: 'class',
+	plugins: [],
+}
