30d9488f6e9fa3cd39b5722080bccc70cd638b0a
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-01-12 12:19:10 +0530

    home page done

--- components/projects/ProjectCard.jsx
+++ components/projects/ProjectCard.jsx
@@ -1,23 +1,30 @@
 import Link from "next/link";
 import Image from "next/image";
-export default function ProjectCard( {keys ,title, description, url,imageSrc,tags,ongoing} ) {
+export default function ProjectCard({
+  keys,
+  title,
+  description,
+  url,
+  imageSrc,
+  tags,
+  ongoing,
+}) {
   return (
     <>
       <Link
         key={keys}
         href={url}
-        target="
-blank" rel=" noopener noreferrer"
-        class="overflow-hidden rounded-2xl bg-gradient-to-b from-gray-300 to-gray-200/50 p-px dark:from-gray-700 dark:via-gray-800 dark:to-darker  sshadow-sm transition hover:shadow-lg  dark:shadow-gray-700/25  group "
+        target="blank"
+        rel=" noopener noreferrer"
+        class="overflow-hidden rounded-2xl bg-gradient-to-b from-gray-300 to-gray-200/50 p-px dark:from-gray-700 dark:via-gray-800 dark:to-darker  sshadow-sm transition hover:shadow-lg  dark:shadow-gray-600/25  group "
       >
-        <div class="relative flex h-full flex-col gap-6 rounded-2xl bg-gray-100 p-2 dark:bg-gray-900">
+        <div class="relative flex h-full flex-col gap-6 rounded-2xl bg-gray-100 p-2 dark:bg-gray-800">
           <article className="font-heading">
             <Image
               alt={title}
               src={imageSrc}
               width={1170}
               height={500}
-              
               className="h-56 w-full rounded-xl object-cover shadow-xl transition  group-hover:scale-[1.01] "
             />
 
@@ -38,11 +45,14 @@ blank" rel=" noopener noreferrer"
               </p>
             </div>
             <div class="flex flex-wrap gap-2 px-4 pt-2 pb-4">
-            {tags.map((tag) => (
-                
-              <span key={tag} class="whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-xs text-blue-600 border border-blue-600">
-                {tag}
-              </span> ))}  
+              {tags.map((tag) => (
+                <span
+                  key={tag}
+                  class="whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-xs text-blue-600 border border-blue-600"
+                >
+                  {tag}
+                </span>
+              ))}
             </div>
           </article>
         </div>
@@ -50,7 +60,8 @@ blank" rel=" noopener noreferrer"
     </>
   );
 }
- {/* <span class="whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-xs text-blue-600">
+{
+  /* <span class="whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-xs text-blue-600">
                 JavaScript
               </span>
               <span class="whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-xs text-blue-600">
@@ -67,4 +78,5 @@ blank" rel=" noopener noreferrer"
               </span>
               <span class="whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-xs text-blue-600">
                 JavaScript
-              </span> */}
+              </span> */
+}
