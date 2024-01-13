71147af431c837369e0d26000d868ea5562e0094
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-01-14 01:31:51 +0530

    some fix

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -2,50 +2,49 @@ import Image from "next/image";
 import { ArrowDownToLine } from "lucide-react";
 export default function AboutMe() {
   return (
-   <>
+    <>
       <div class="lg:bg-gray-200/50 dark:lg:bg-gray-800 lg:p-10 rounded-[4rem] space-y-6 md:flex md:gap-6 justify-center md:space-y-0 lg:items-center mt-20">
-          <div class="md:w-5/12 lg:w-1/3">
-            <div id="cards" class="rounded-3xl md:-mx-5">
-              <div class="card rounded-3xl" data-color="brown">
-                <Image
-                  tabindex="0"
-                  class="card-front-image h-80 md:h-96 card-image rounded-3xl border border-black dark:border-white "
-                  alt="profile Image"
-                  priority={true}
-                  width="300"
-                  height="300"
-                  src="/profile-pic-final.jpg"
-                  quality={25}
-                />
-              </div>
+        <div class="md:w-5/12 lg:w-1/3">
+          <div id="cards" class="rounded-3xl md:-mx-5">
+            <div class="card rounded-3xl" data-color="brown">
+              <Image
+                tabindex="0"
+                class="card-front-image h-80 md:h-96 card-image rounded-3xl border border-black dark:border-white "
+                alt="profile Image"
+                priority={true}
+                width={300}
+                height={375}
+                src="/profile-pic-final.jpg"
+                quality={25}
+              />
             </div>
           </div>
-          <div class="md:w-7/12 lg:w-2/3">
-            <h2 class="text-2xl md:text-3xl lg:text-4xl pt-4 font-bold">
-              So, Who Am I?
-            </h2>
-            <p class="my-4 text-gray-600 dark:text-gray-300 gotham">
-              A web development enthusiast currently pursuing a B.Tech in CSE.
-              I'm all about coding, design, and, of course music. As an
-              individual, I take pride in my laid-back and down-to-earth nature.
-              I approach life with an open mind and a welcoming spirit, always
-              eager to embrace fresh challenges and opportunities who loves
-              taking on new challenges.
-            </p>
-
-            <a
-              href="/resume.pdf"
-              aria-label="Download Resume"
-              className="group inline-flex py-2 items-center gap-2.5 rounded-md bg-gray-300 hover:bg-gray-300/70 px-4 transition-all duration-500 dark:bg-gray-600 dark:hover:bg-gray-700"
-            >
-              <ArrowDownToLine />
-              <span className="text-lg  lg:text-xl font-medium tracking-normal ">
-                Resume
-              </span>
-            </a>
-          </div>
         </div>
-   
-   </>
-  )
+        <div class="md:w-7/12 lg:w-2/3">
+          <h2 class="text-2xl md:text-3xl lg:text-4xl pt-4 font-bold">
+            So, Who Am I?
+          </h2>
+          <p class="my-4 text-gray-600 dark:text-gray-300 gotham">
+            A web development enthusiast currently pursuing a B.Tech in CSE. I'm
+            all about coding, design, and, of course music. As an individual, I
+            take pride in my laid-back and down-to-earth nature. I approach life
+            with an open mind and a welcoming spirit, always eager to embrace
+            fresh challenges and opportunities who loves taking on new
+            challenges.
+          </p>
+
+          <a
+            href="/resume.pdf"
+            aria-label="Download Resume"
+            className="group inline-flex py-2 items-center gap-2.5 rounded-md bg-gray-300 hover:bg-gray-300/70 px-4 transition-all duration-500 dark:bg-gray-600 dark:hover:bg-gray-700"
+          >
+            <ArrowDownToLine />
+            <span className="text-lg  lg:text-xl font-medium tracking-normal ">
+              Resume
+            </span>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
--- components/TechStacks.jsx
+++ components/TechStacks.jsx
@@ -9,17 +9,18 @@ export default function TechStacks() {
         </h2>
         <p class=" pt-5 text-base text-gray-600">Tools and Frameworks I use</p>
 
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5 py-5 pb-20">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5 lg:gap-10 py-5 pb-20">
           {TechStack.map((tech, index) => (
             <div
               key={index}
-              className="flex space-x-2 items-center justify-center px-2 md:px-3 py-2 rounded-lg bg-gray-200  dark:bg-gray-700"
+              className="flex space-x-2 items-center justify-center px-2 md:px-3 py-2 rounded-lg bg-gray-300  dark:bg-gray-600"
             >
               <Image
                 src={tech.logo}
-                width="50"
-                height="50"
-                className="h-8 md:h-10"
+                alt={tech.name}
+                width={50}
+                height={50}
+                className="h-8 w-8 md:h-10 md:w-10 p-1 bg-white rounded-xl"
               />
               <span className=" text-sm md:text-lg font-semibold ">
                 {" "}
