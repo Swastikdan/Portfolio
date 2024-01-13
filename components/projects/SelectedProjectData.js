035575ac91b3bb9143cadef31475269b532180e4
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-01-14 01:11:16 +0530

    image change

--- components/projects/SelectedProjectData.js
+++ components/projects/SelectedProjectData.js
@@ -1,22 +1,22 @@
 const SelectedProjectData = [
-    
+
     {
         title: "Stayz - Booking Website",
         description: "Find, compare, and reserve your ideal hotel.",
         url: "https://stayz.vercel.app",
-        imageSrc: "/images/project-image/stayz.vercel.app.png",
-        tags: ["Next Js", "Javascript", "Clerk Auth", "Mongo DB","Tailwind","shadcn/ui","Swiper Js" ],
+        imageSrc: "/images/project-image/stayz.vercel.app.jpg",
+        tags: ["Next Js", "Javascript", "Clerk Auth", "Mongo DB", "Tailwind", "shadcn/ui", "Swiper Js"],
         id: 2,
-        ongoing:true,
+        ongoing: true,
 
-    },{
+    }, {
         title: "E-Commerce Website",
         description: "E-Commerce Web app built using Django",
         url: "https://swastikpyprojects.pythonanywhere.com/",
-        imageSrc: "/images/project-image/swastikpyprojects.pythonanywhere.com.png",
-        tags: ["Django", "Tailwind", "JavaScript", "SQLite", "Python" ,"Swiper Js"],
+        imageSrc: "/images/project-image/swastikpyprojects.pythonanywhere.com.jpg",
+        tags: ["Django", "Tailwind", "JavaScript", "SQLite", "Python", "Swiper Js"],
         id: 1,
-        ongoing:false,
+        ongoing: false,
     },
     {
         title: "Recipe Seeker Webapp",
@@ -25,9 +25,9 @@ const SelectedProjectData = [
         imageSrc: "/images/project-image/racipe-seeker.swastikdan.in.png",
         tags: ["Node Js", "Express Js", "Mongo DB", "Tailwind", "JavaScript"],
         id: 3,
-        ongoing:false,
+        ongoing: false,
     },
-    
+
 ];
 
 export default SelectedProjectData;
