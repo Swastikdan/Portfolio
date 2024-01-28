4c77dfe5acc00f34ad93c91bbc9e7cea53cb8acf
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-01-28 19:42:29 +0530

    project update

--- components/projects/ProjectPageCardTrigger.jsx
+++ components/projects/ProjectPageCardTrigger.jsx
@@ -1,43 +1,33 @@
-"use client";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import AllProjectData from "./AllProjectData";
 import Image from "next/image";
+import { Eye, Github } from "lucide-react";
+import Link from "next/link";
 export default function ProjectPageCardTrigger() {
   return (
     <>
       {AllProjectData.map((project) => (
-        <Dialog>
-          <DialogTrigger>
-            
-            <li className=" transition-transform duration-300 p-3 rounded hover:scale-105 hover:border-4   border-gray-600 dark:border-gray-400 -m-5 ">
-              <div class="grid items-center gap-5 md:grid-cols-2 md:gap-10">
+     
+            <li className=" transition-transform duration-300 hover:bg-gray-900/50 py-4 px-8 rounded-md  ">
+              <div className="grid items-center gap-5 md:grid-cols-2 md:gap-10">
                 <Image
                   src={project.imageSrc}
                   sizes="(max-width: 800px) 100vw, 800px"
                   loading="eager"
                   decoding="sync"
-                  class="w-full rounded-md bg-white object-cover object-center"
+                  className="w-full rounded-md bg-white object-cover object-center"
                   width="800"
                   height="600"
                 />
                 <div>
                   {project.ongoing ? (
-                    <span class="text-green-400 u text-sm  flex">
-                      
+                    <span className="text-green-400 text-start px-2 md:px-0 text-sm  flex">
                       Ongoing
                     </span>
                   ) : (
                     ""
                   )}
 
-                  <h2 class="mt-1  text-center md:text-start text-2xl lg:text-3xl font-semibold ">
+                  <h2 className="mt-1  text-center md:text-start text-2xl lg:text-3xl font-semibold ">
                     {project.title}
                   </h2>
                   <p className="text-gray-600 dark:text-gray-400 text-center md:text-start py-2">
@@ -48,28 +38,33 @@ export default function ProjectPageCardTrigger() {
                       {project.tags.map((tag) => (
                         <span
                           key={tag}
-                          class="text-xs  tracking-wider  text-blue-600 dark:text-blue-400 ring-1 px-1.5 md:px-2 py-0.5 text-[10px] md:text-xs rounded"
+                          className="text-xs  tracking-wider  text-blue-600 dark:text-blue-400 ring-1 px-1.5 md:px-2 py-0.5 text-[10px] md:text-xs rounded"
                         >
-                          
                           {tag}
                         </span>
                       ))}
                     </div>
                   </div>
+                  <div className="flex justify-center md:justify-start  space-x-2 pt-4 text-xs  md:text-sm ">
+                  <Link
+                    href={project.url}
+                    target="_blank"
+                    className="flex gap-1 ring-1 ring-black dark:ring-white px-2 py-1 rounded-md opacity-80 hover:opacity-100 active:scale-95 items-center "
+                  >
+                    <Eye className="p-1" /> Live Demo
+                  </Link>
+                  <Link
+                    href={project.github}
+                    target="_blank"
+                    className={`flex gap-2 ring-1 ring-black dark:ring-white px-2 py-1 rounded-md opacity-80 hover:opacity-100 active:scale-95 items-center ${!project.github ? "hidden" : ""}`}
+                  >
+                    <Github className="p-1" /> Source Code
+                  </Link>
+                </div>
                 </div>
               </div>
             </li>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Are you absolutely sure?</DialogTitle>
-              <DialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
+         
       ))}
     </>
   );
