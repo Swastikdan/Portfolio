2640f0d728b6a1404b83f9628365dd21a794f0c9
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-06-13 22:11:39 +0530

    techstack  fix

--- components/TechStack.js
+++ components/TechStack.js
@@ -35,19 +35,19 @@
 // Vercel;
 
 const TechStack = [
-  { title: "Java", logo: "/images/icons/java.svg" },
-  { title: "JavaScript", logo: "/images/icons/javascript.svg" },
-  { title: "HTML", logo: "/images/icons/html.svg" },
-  { title: "CSS", logo: "/images/icons/css.svg" },
-  { title: "Tailwind", logo: "/images/icons/tailwindcss-light.svg" },
-  { title: "React", logo: "/images/icons/react.svg" },
-  { title: "Redux", logo: "/images/icons/redux.svg" },
-  { title: "Node", logo: "/images/icons/nodejs.svg" },
-  { title: "Express", logo: "/images/icons/expressjs-icon.svg" },
-  { title: "MongoDB", logo: "/images/icons/mongo.svg" },
-  { title: "Supabase", logo: "/images/icons/supabase.svg" },
-  { title: "SQL", logo: "/images/icons/sql.svg" },
-  { title: "Git", logo: "/images/icons/git.svg" },
+  { title: 'Java', logo: '/images/icons/java.svg' },
+  { title: 'JavaScript', logo: '/images/icons/javascript.svg' },
+  { title: 'HTML', logo: '/images/icons/html.svg' },
+  { title: 'CSS', logo: '/images/icons/css.svg' },
+  { title: 'Tailwind', logo: '/images/icons/tailwindcss-light.svg' },
+  { title: 'React', logo: '/images/icons/react.svg' },
+  // { title: "Redux", logo: "/images/icons/redux.svg" },
+  { title: 'Node', logo: '/images/icons/nodejs.svg' },
+  { title: 'Express', logo: '/images/icons/expressjs-icon.svg' },
+  { title: 'MongoDB', logo: '/images/icons/mongo.svg' },
+  // { title: "Supabase", logo: "/images/icons/supabase.svg" },
+  { title: 'SQL', logo: '/images/icons/sql.svg' },
+  { title: 'Git', logo: '/images/icons/git.svg' },
 ];
 
 export default TechStack;
